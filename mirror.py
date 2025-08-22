f4aa80274b07bc48aab04fbe858344164a6a7c32
Jashwanth G <codesjash@gmail.com>
2025-08-22 15:10:11 +0530

    initial commit

---
+++ mirror.py
@@ -0,0 +1,44 @@
+# n = 4
+# for i in range(1,n+1):
+#     for sp in range(n+1,i,-1):
+#         print(" ",end="")
+#     for j in range(1,i+1):
+#         print("*", end=" ")
+#     print()
+
+# for i in range(n-1,0,-1):
+#     for sp in range(n+1,i,-1):
+#             print(" ",end="")
+#     for j in range(1,i+1):
+#         print("*", end=" ")
+#     print()
+
+
+
+n = 4
+for i in range(1,n+1):
+    for sp in range(n+1,i,-1):
+        print(" ",end="")
+    for j in range(1,i+1):
+        print("*", end=" ")
+    print("",end="")
+
+    for sp in range(n+1,i,-1):
+        print(" ",end=" ")
+    for j in range(1,i+1):
+        print("*", end=" ")
+    print()
+
+for i in range(n-1,0,-1):
+    for sp in range(n+1,i,-1):
+            print(" ",end="")
+    for j in range(1,i+1):
+        print("*", end=" ")
+    print("", end="")
+    for sp in range(n+1,i,-1):
+            print(" ",end=" ")
+    for j in range(1,i+1):
+        print("*", end=" ")
+    print()
+  
+
---
+++ sample.py
@@ -0,0 +1,9 @@
+num = int(input("enter a num :"))
+if num%2 != 0:
+    print("Weird")
+elif num%2 == 0 and 2 <= num <=5:
+    print("Not Weird")
+elif num%2 == 0 and 6 <= num <=20:
+    print("Weird")
+elif num%2 == 0 and num>20:
+    print("Not Weird")
