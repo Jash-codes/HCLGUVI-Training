b10edc383663a6ca389d0d2a8d0dc09ca87a98f8
Jashwanth G <codesjash@gmail.com>
2025-09-08 09:27:34 +0530

    intial commit

---
+++ dec.py
@@ -0,0 +1,17 @@
+def Honda(a,b):
+    print("got 32%+")
+    def CBR600(x):
+        price = a + b + x
+        return price
+    
+    return CBR600
+
+f = Honda(2,2)
+rez = f(2)
+
+print(rez)
+
+
+
+    
+
